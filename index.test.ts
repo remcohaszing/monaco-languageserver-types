39ca69a80e37870430e547574edd43afbe65da52
Remco Haszing <remcohaszing@gmail.com>
2023-02-26 13:57:43 +0100

    Add support for textEdit

--- index.test.ts
+++ index.test.ts
@@ -524,3 +524,20 @@ runTests({
   to: fns.toSymbolTag,
   tests: [{ lsp: ls.SymbolTag.Deprecated, monaco: monaco.languages.SymbolTag.Deprecated }]
 })
+
+runTests({
+  from: fns.fromTextEdit,
+  to: fns.toTextEdit,
+  tests: [
+    {
+      lsp: {
+        newText: 'New text',
+        range: { start: { line: 0, character: 10 }, end: { line: 2, character: 20 } }
+      },
+      monaco: {
+        range: { startLineNumber: 1, startColumn: 11, endLineNumber: 3, endColumn: 21 },
+        text: 'New text'
+      }
+    }
+  ]
+})
---
+++ lib/textEdit.ts
@@ -0,0 +1,30 @@
+import type * as monaco from 'monaco-editor'
+import type * as ls from 'vscode-languageserver-types'
+
+import { fromRange, toRange } from './range.js'
+
+/**
+ * Convert a Monaco editor text edit to an LSP text edit.
+ *
+ * @param textEdit The Monaco text edit to convert.
+ * @returns The text edit as an LSP text edit.
+ */
+export function fromTextEdit(textEdit: monaco.languages.TextEdit): ls.TextEdit {
+  return {
+    range: fromRange(textEdit.range),
+    newText: textEdit.text
+  }
+}
+
+/**
+ * Convert an LSP text edit to a Monaco editor text edit.
+ *
+ * @param textEdit The LSP text edit to convert.
+ * @returns The text edit as Monaco editor text edit.
+ */
+export function toTextEdit(textEdit: ls.TextEdit): monaco.languages.TextEdit {
+  return {
+    range: toRange(textEdit.range),
+    text: textEdit.newText
+  }
+}
