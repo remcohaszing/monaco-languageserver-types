73e922b6d692038b3901d539a17450cecdc1d7ed
Remco Haszing <remcohaszing@gmail.com>
2023-03-06 16:30:14 +0100

    Support completion list

---
+++ test/completionList.test.ts
@@ -0,0 +1,70 @@
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js'
+import * as ls from 'vscode-languageserver-types'
+
+import { runTests } from './utils.js'
+import { fromCompletionList, toCompletionList } from '../index.js'
+
+runTests(fromCompletionList, toCompletionList)(
+  {
+    toOptions: {
+      range: { startLineNumber: 999, startColumn: 999, endLineNumber: 999, endColumn: 999 }
+    },
+    lsp: {
+      isIncomplete: false,
+      items: [
+        {
+          kind: ls.CompletionItemKind.Class,
+          label: 'Insert "text"',
+          textEdit: {
+            newText: 'text',
+            range: { start: { line: 0, character: 1 }, end: { line: 2, character: 3 } }
+          }
+        }
+      ]
+    },
+    monaco: {
+      incomplete: false,
+      suggestions: [
+        {
+          insertText: 'text',
+          kind: monaco.languages.CompletionItemKind.Class,
+          label: 'Insert "text"',
+          range: { startLineNumber: 1, startColumn: 2, endLineNumber: 3, endColumn: 4 }
+        }
+      ]
+    }
+  },
+  {
+    only: 'to',
+    toOptions: {
+      range: { startLineNumber: 999, startColumn: 999, endLineNumber: 999, endColumn: 999 }
+    },
+    lsp: {
+      isIncomplete: false,
+      itemDefaults: {
+        commitCharacters: ['"'],
+        editRange: { start: { line: 0, character: 1 }, end: { line: 2, character: 3 } },
+        insertTextFormat: ls.InsertTextFormat.Snippet
+      },
+      items: [
+        {
+          kind: ls.CompletionItemKind.Class,
+          label: 'Insert "text"'
+        }
+      ]
+    },
+    monaco: {
+      incomplete: false,
+      suggestions: [
+        {
+          commitCharacters: ['"'],
+          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          insertText: '',
+          kind: monaco.languages.CompletionItemKind.Class,
+          label: 'Insert "text"',
+          range: { startLineNumber: 1, startColumn: 2, endLineNumber: 3, endColumn: 4 }
+        }
+      ]
+    }
+  }
+)
