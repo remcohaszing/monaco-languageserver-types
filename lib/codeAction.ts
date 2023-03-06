d00dac7b3f1126650f9a860680537f597fb59493
Remco Haszing <remcohaszing@gmail.com>
2023-03-06 17:38:57 +0100

    Support default options when converting code actions

--- lib/codeAction.ts
+++ lib/codeAction.ts
@@ -25,16 +25,29 @@ export function fromCodeAction(codeAction: monaco.languages.CodeAction): ls.Code
   }
 }
 
+export interface ToCodeActionOptions {
+  /**
+   * The default severity in case it’s not provided by a diagnostic.
+   *
+   * @default MarkerSeverity.Error
+   */
+  defaultSeverity?: monaco.MarkerSeverity
+}
+
 /**
  * Convert an LSP code action to a Monaco editor code action.
  *
  * @param codeAction The LSP code action to convert.
+ * @param options Additional options needed to construct the Monaco code action.
  * @returns The code action as Monaco editor code action.
  */
-export function toCodeAction(codeAction: ls.CodeAction): monaco.languages.CodeAction {
+export function toCodeAction(
+  codeAction: ls.CodeAction,
+  options?: ToCodeActionOptions
+): monaco.languages.CodeAction {
   return {
     title: codeAction.title,
-    diagnostics: codeAction.diagnostics?.map(toMarkerData),
+    diagnostics: codeAction.diagnostics?.map((diagnostic) => toMarkerData(diagnostic, options)),
     disabled: codeAction.disabled?.reason,
     edit: codeAction.edit ? toWorkspaceEdit(codeAction.edit) : undefined,
     kind: codeAction.kind,
