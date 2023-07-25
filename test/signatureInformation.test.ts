4c92331b639a787656bcd4046269f0e14bace2e0
Remco Haszing <remcohaszing@gmail.com>
2023-07-25 17:13:31 +0200

    Support signature information

---
+++ test/signatureInformation.test.ts
@@ -0,0 +1,30 @@
+import { runTests } from './utils.js'
+import { fromSignatureInformation, toSignatureInformation } from '../index.js'
+
+runTests(fromSignatureInformation, toSignatureInformation)(
+  {
+    lsp: { label: 'Label', parameters: [] },
+    monaco: { label: 'Label', parameters: [] }
+  },
+  {
+    only: 'to',
+    lsp: { label: 'Label' },
+    monaco: { label: 'Label', parameters: [] }
+  },
+  {
+    lsp: { label: 'Label', documentation: '', parameters: [] },
+    monaco: { label: 'Label', documentation: '', parameters: [] }
+  },
+  {
+    lsp: { label: 'Label', documentation: { value: '', kind: 'markdown' }, parameters: [] },
+    monaco: { label: 'Label', documentation: { value: '' }, parameters: [] }
+  },
+  {
+    lsp: { label: 'Label', parameters: [{ label: 'Parameter' }] },
+    monaco: { label: 'Label', parameters: [{ label: 'Parameter' }] }
+  },
+  {
+    lsp: { label: 'Label', activeParameter: 0, parameters: [] },
+    monaco: { label: 'Label', activeParameter: 0, parameters: [] }
+  }
+)
